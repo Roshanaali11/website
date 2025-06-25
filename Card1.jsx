d708c18aba17c40cbf7a9a016346907afe7d2d0e
Roshanaali11 <roshanaali2020@gmail.com>
2025-06-25 15:17:27 +0530

    Add files via upload

---
+++ Card1.jsx
@@ -0,0 +1,16 @@
+import pic from './assets/oilfuel.webp'
+
+function Card1 (){
+
+return(
+<div className="card1"> 
+<img className='picture1' src={pic}></img>
+<h2>Oil Fuel Cells</h2>
+<p>Detailed description of the service provided here</p>
+</div>
+
+);
+
+}
+
+export default Card1
